3ebdfdec9e2de582c507aad908d59a8c62921c22
Azeem Shaik <azeemshaik025@gmail.com>
2024-06-01 15:17:21 +0530

    Create complete website

--- scripts.js
+++ scripts.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
+
+    // Toggling the visibility of contact us form
+
     $(".toggle-form").click(function () {
-        console.log('hel');
         $("#contact-us-form").toggleClass("show-contact-form");
     });
+
+
 });
+
+// Sliding the images in what we do section
+
+function moveImage(value) {
+
+    for (let i = 1; i < 4; i++) {
+        $(`.sec${i}`).removeClass("active-sec");
+        $(`.sec${i}-dot`).removeClass("active");
+    }
+    $(`.sec${value}`).toggleClass("active-sec");
+    $(`.sec${value}-dot`).toggleClass("active");
+}
+
+//Changing image based on option
+
+function changeImage(value) {
+    const element = $(".project-image");
+    console.log(element[0]);
+    element[0].src = `./assets/${value}.png`;
+}
